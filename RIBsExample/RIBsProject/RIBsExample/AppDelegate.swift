089a2764a4a51ea87c403d7a2470048d8e91623b
고대원 <yreak33@gmail.com>
2020-01-10 22:56:00 +0900

    init

---
+++ RIBsExample/RIBsProject/RIBsExample/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  RIBsExample
+//
+//  Created by 고대원 on 2020/01/07.
+//  Copyright © 2020 고대원. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
---
+++ RIBsExample/RIBsProject/RIBsExample/Login/LoginBuilder.swift
@@ -0,0 +1,40 @@
+//
+//  LoginBuilder.swift
+//  RIBsExample
+//
+//  Created by 고대원 on 2020/01/08.
+//  Copyright © 2020 고대원. All rights reserved.
+//
+
+import RIBs
+
+protocol LoginDependency: Dependency {
+    // TODO: Declare the set of dependencies required by this RIB, but cannot be
+    // created by this RIB.
+}
+
+final class LoginComponent: Component<LoginDependency> {
+
+    // TODO: Declare 'fileprivate' dependencies that are only used by this RIB.
+}
+
+// MARK: - Builder
+
+protocol LoginBuildable: Buildable {
+    func build(withListener listener: LoginListener) -> LoginRouting
+}
+
+final class LoginBuilder: Builder<LoginDependency>, LoginBuildable {
+
+    override init(dependency: LoginDependency) {
+        super.init(dependency: dependency)
+    }
+
+    func build(withListener listener: LoginListener) -> LoginRouting {
+        let component = LoginComponent(dependency: dependency)
+        let viewController = LoginViewController()
+        let interactor = LoginInteractor(presenter: viewController)
+        interactor.listener = listener
+        return LoginRouter(interactor: interactor, viewController: viewController)
+    }
+}
